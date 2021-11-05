6446db416fea927588c32f35110e5f385e80197a
Matt Hardy <corporalhardy@gmail.com>
2021-11-05 16:43:53 -0700

    First commit

---
+++ Python_practice.py
@@ -0,0 +1,56 @@
+print("Hello World")
+
+
+counties_dict = {}
+
+counties_dict["Arapahoe"] = 422829
+counties_dict["Denver"] = 463353
+counties_dict["Jefferson"] = 432438
+
+counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not in the list of counties.")
+
+if "Arapahoe" in counties and "El Paso" in counties:
+    print("Arapahoe and El Paso are in the list of counties.")
+else:
+    print("Arapahoe or El Paso is not in the list of counties.")
+
+for county in counties:
+    print(county)
+
+for i in range(len(counties)):
+    print(counties[i])
+
+for voters in counties_dict.values():
+    print(voters)
+
+for county, voters in counties_dict.items():
+    print(county, voters)
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+for county_dict in voting_data:
+    print(county_dict)
+
+for county_dict in voting_data:
+    for value in county_dict.values():
+        print(value)
+
+for county_dict in voting_data:
+    print(county_dict['county'])
+
+counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters:,} registered voters.")
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+for vote_dict in voting_data:
+    print(f"{vote_dict['county']} county has {vote_dict['registered_voters']:,} registered voters.")
